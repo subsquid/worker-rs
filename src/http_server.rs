12cbd1587ff22f4c90e14b269be7e0fab4875694
Dzmitry Kalabuk <d.kalabuk@subsquid.io>
2024-04-25 11:06:36 +0300

    Set correct content type for the /metrics endpoint

--- src/http_server.rs
+++ src/http_server.rs
@@ -7,6 +7,7 @@ use crate::{
 
 use axum::{
     extract::Path,
+    http::{header, HeaderMap},
     response::{IntoResponse, Response},
     routing::{get, post},
     Json,
@@ -38,10 +39,24 @@ async fn run_query(
         .into_response()
 }
 
-async fn get_metrics(registry: Arc<Registry>) -> String {
+async fn get_metrics(registry: Arc<Registry>) -> impl IntoResponse {
+    lazy_static::lazy_static! {
+        static ref HEADERS: HeaderMap = {
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                header::CONTENT_TYPE,
+                "application/openmetrics-text; version=1.0.0; charset=utf-8"
+                    .parse()
+                    .unwrap(),
+            );
+            headers
+        };
+    }
+
     let mut buffer = String::new();
     encode(&mut buffer, &registry).unwrap();
-    buffer
+
+    (HEADERS.clone(), buffer)
 }
 
 pub struct Server {
