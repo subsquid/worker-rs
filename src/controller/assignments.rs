6fc9581edbd92fc7298c41547785164db84cc9ec
Adam Wierzbicki <adam.wierzbicki@nearone.org>
2025-08-12 11:19:33 +0200

    Don't interrupt assignment stream on error

--- src/controller/assignments.rs
+++ src/controller/assignments.rs
@@ -1,7 +1,7 @@
 use std::{io::ErrorKind, time::Duration};
 
-use async_stream::try_stream;
-use futures::{Stream, StreamExt};
+use async_stream::stream;
+use futures::Stream;
 use tokio::time::MissedTickBehavior;
 
 pub struct AssignmentUpdate {
@@ -22,47 +22,43 @@ pub fn new_assignments_stream(
 
     let mut last_id = None;
 
-    let with_errs = try_stream! {
+    stream! {
         loop {
             timer.tick().await;
             tracing::debug!("Checking for new assignment");
-            let network_state = fetch_network_state(&url, &reqwest_client).await?;
+            let Ok(network_state) = fetch_network_state(&url, &reqwest_client).await.map_err(
+                |e| tracing::warn!(error = %e, "Failed to update assignment, waiting for the next one"),
+            ) else {
+                continue;
+            };
             let assignment_id = network_state.assignment.id;
             if last_id.as_ref() == Some(&assignment_id) {
                 tracing::debug!("Assignment has not been changed");
                 continue;
             }
-
+    
+            let Some(url) = &network_state.assignment.fb_url else {
+                tracing::warn!("Missing fb_url");
+                continue;
+            };
+    
             tracing::debug!("Downloading assignment \"{}\"", assignment_id);
-            let assignment = fetch_assignment(
-                &network_state
-                    .assignment
-                    .fb_url
-                    .ok_or(anyhow::anyhow!("Missing fb_url"))?,
-                &reqwest_client,
-            )
-            .await?;
+            let Ok(assignment) = fetch_assignment(url, &reqwest_client).await.map_err(
+                |e| tracing::warn!(error = %e, "Failed to update assignment, waiting for the next one"),
+            ) else {
+                continue;
+            };
             last_id = Some(assignment_id.clone());
-
+    
             tracing::debug!("Downloaded assignment \"{}\"", assignment_id);
-
+    
             yield AssignmentUpdate {
                 assignment,
                 id: assignment_id,
                 effective_from: network_state.assignment.effective_from,
             };
         }
-    };
-
-    with_errs.filter_map(|res: anyhow::Result<AssignmentUpdate>| async move {
-        match res {
-            Ok(data) => Some(data),
-            Err(e) => {
-                tracing::warn!(error = %e, "Failed to update assignment, waiting for the next one");
-                None
-            }
-        }
-    })
+    }
 }
 
 async fn fetch_network_state(
